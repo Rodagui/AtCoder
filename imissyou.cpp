0f382a32bab6fa3784ab757b061df97cdd8b78c2
Kathia Rodríguez <39465484+Rodagui@users.noreply.github.com>
2020-02-20 19:45:48 -0600

    Add files via upload

---
+++ imissyou.cpp
@@ -0,0 +1,17 @@
+/*B - I miss you...*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	
+	string cad;
+	cin >> cad;
+
+	for (int i = 0; i < cad.size(); ++i)
+		cout << "x";
+	
+
+	return 0;
+}
