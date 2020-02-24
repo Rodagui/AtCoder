28be98e2f1b84df81070009431535148a24f7482
Kathia Rodríguez <39465484+Rodagui@users.noreply.github.com>
2020-02-24 12:22:50 -0600

    Add files via upload

---
+++ cantWaitforHoliday.cpp
@@ -0,0 +1,34 @@
+/*A - Can't Wait for Holiday*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+	string cad;
+
+	cin >> cad;
+
+	if(cad == "SUN")
+		cout << '7';
+
+	if(cad == "MON")
+		cout << '6';
+
+	if(cad == "TUE")
+		cout << '5';
+
+	if(cad == "WED")
+		cout << '4';
+
+	if(cad == "THU")
+		cout << '3';
+
+	if(cad == "FRI")
+		cout << '2';
+
+	if(cad == "SAT")
+		cout << '1';
+
+	return 0;
+}
