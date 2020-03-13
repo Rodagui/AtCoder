d6a0d27c59719891a1222b00d05cc445bc222f62
Kathia Rodríguez <39465484+Rodagui@users.noreply.github.com>
2020-03-12 19:31:34 -0600

    Add files via upload

---
+++ password.cpp
@@ -0,0 +1,23 @@
+/*A - Password*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	int ans = 1;
+
+	ans = n * n * n;
+
+	cout << ans;
+
+	return 0;
+}
