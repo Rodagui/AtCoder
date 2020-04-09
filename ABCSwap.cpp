f6526c919dbc20259055f32eaa993675b38efb5f
Kathia Rodríguez <39465484+Rodagui@users.noreply.github.com>
2020-04-08 19:11:24 -0500

    Add files via upload

---
+++ ABCSwap.cpp
@@ -0,0 +1,16 @@
+/*A - ABC Swap*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	
+	int A, B, C;
+
+	cin >> A >> B >> C;
+
+	cout << C << ' ' << A << ' ' << B;
+
+	return 0;
+}
