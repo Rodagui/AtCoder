dace259f5c8ccce3867f4e07ee413667ce6e6b6f
Kathia Rodríguez <39465484+Rodagui@users.noreply.github.com>
2020-04-20 15:01:28 -0500

    Add files via upload

---
+++ circlePond.cpp
@@ -0,0 +1,19 @@
+/*A - Circle Pond*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	double radio;
+	cin >> radio;
+
+	cout << fixed << setprecision(20) << M_PI * (radio + radio) << endl;
+
+	
+	return 0;
+}
